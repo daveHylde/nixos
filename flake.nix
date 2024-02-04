210aa40d40bfb0ae144ae18d4d40839df90fa969
daveHylde <david@jobbi.no>
2024-02-04 21:51:20 +0100

    flakes

--- flake.nix
+++ flake.nix
@@ -1,11 +1,35 @@
 {
-  description = "A very basic flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  }: 
+    let
+      user = "david";
+    in {
+    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs self user;};
+      modules = [
+        ./system/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.david = import ./home-manager/home.nix;
+          home-manager.extraSpecialArgs = {inherit inputs self user;};
+        }
+      ];
+    };
 
   };
 }
