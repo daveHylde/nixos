b48d0e62f1b58322158b1f0c4cd8e3406a2a36e1
daveHylde <david@jobbi.no>
2024-02-06 20:05:44 +0100

    nvim

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -58,5 +58,48 @@ in {
       xclip
       yarn
     ];
+  programs.neovim =
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./neovim/options.lua}
+    '';
+
+    extraPackages = with pkgs; [
+      lua-language-server
+      xclip
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+      telescope-nvim
+      
+      (nvim-treesitter.withPlugins (p: [
+	p.tree-sitter-nix
+	p.tree-sitter-bash
+	p.tree-sitter-lua
+	p.tree-sitter-json
+	p.tree-sitter-c-sharp
+	p.tree-sitter-typescript
+	p.tree-sitter-javascript
+	p.tree-sitter-html
+	p.tree-sitter-css
+	p.tree-sitter-scss
+	p.tree-sitter-dockerfile
+	p.tree-sitter-yaml
+      ]))
+
+      vim-nix
+    ];
   };
+ };
 }
