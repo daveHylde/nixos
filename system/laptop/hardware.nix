4939cf4b20251fac6ea11a9ad2410a2ed591037e
daveHylde <david@jobbi.no>
2024-05-29 10:39:04 +0200

    desktop

--- system/laptop/hardware.nix
+++ system/laptop/hardware.nix
@@ -40,6 +40,13 @@
     thinkfan = {
       enable = true;
     };
+    fprintd = {
+      enable = true;
+      tod = {
+        enable = true;
+        driver = pkgs.libfprint-2-tod1-goodix;
+      };
+    };
   };
 
   hardware = {
