06b638989b1fa43108262f46fb70012c51182c5d
daveHylde <david@jobbi.no>
2024-08-18 17:46:57 +0200

    connect

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,52 +33,53 @@
 		};
 
     packages = [
-      pkgs.clinfo
+			pkgs-unstable.figma-linux
+			pkgs-unstable.netcoredbg
+			pkgs-unstable.signal-desktop
+			pkgs.fzf
+			pkgs.k9s
+			pkgs.kdePackages.kdeconnect-kde
+			pkgs.xwayland
+			pkgs.yubikey-manager-qt
+      pkgs-unstable.lua-language-server
+      pkgs-unstable.openlens
+      pkgs-unstable.openrgb
+      pkgs-unstable.slack
+      pkgs-unstable.spotify
+      pkgs-unstable.thunderbird
+      pkgs-unstable.vscode-fhs
       pkgs.cargo
+      pkgs.chromium
+      pkgs.clinfo
       pkgs.dotnetCorePackages.sdk_8_0
       pkgs.firefox
       pkgs.flameshot
-			pkgs.fzf
-      pkgs.git
       pkgs.gimp
+      pkgs.git
       pkgs.glib
       pkgs.glxinfo
       pkgs.jq
-      pkgs.chromium
-			pkgs-unstable.figma-linux
-			pkgs.k9s
-      pkgs.kitty
       pkgs.keepassxc
+      pkgs.kitty
       pkgs.kubectl
       pkgs.kubernetes-helm
       pkgs.lazydocker
-      pkgs-unstable.lua-language-server
       pkgs.libreoffice
       pkgs.lm_sensors
       pkgs.minikube
       pkgs.mysql80
       pkgs.nerdfonts
-			pkgs-unstable.netcoredbg
       pkgs.nodejs
-      pkgs-unstable.openlens
-      pkgs-unstable.openrgb
       pkgs.openssl
       pkgs.powershell
       pkgs.python3Full
-			pkgs-unstable.signal-desktop
-      pkgs-unstable.slack
-      pkgs-unstable.spotify
       pkgs.tldr
-      pkgs-unstable.thunderbird
-      pkgs-unstable.vscode-fhs
+      pkgs.unzip
       pkgs.wl-clipboard
       pkgs.yarn
-			pkgs.yubikey-manager-qt
+      pkgs.yq
       pkgs.yubikey-personalization
       pkgs.yubioath-flutter
-      pkgs.yq
-      pkgs.unzip
-			pkgs.xwayland
       pkgs.zip
     ];
   };
