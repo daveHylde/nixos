1dfd9c5f906063b18e8dab764e5ba6cd4f9ac22d
daveHylde <david@jobbi.no>
2024-09-13 14:11:22 +0200

    ollama

--- system/amdgpu.nix
+++ system/amdgpu.nix
@@ -13,4 +13,15 @@
 			rocmPackages.clr.icd
 		];
 	};
+
+	services = {
+		ollama = {
+			enable = true;
+			acceleration = "rocm";
+			environmentVariables = {
+				HCC_AMDGPU_TARGET = "gfx1031";
+			};
+			rocmOverrideGfx = "10.3.1";
+		};
+	};
 }
