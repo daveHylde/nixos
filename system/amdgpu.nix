8b31d03a617f16192475a1f2685ed835a2f30ba0
daveHylde <david@jobbi.no>
2024-09-20 14:28:29 +0200

    amd

--- system/amdgpu.nix
+++ system/amdgpu.nix
@@ -2,17 +2,16 @@
 
 {
 	hardware.graphics = {
-
-		# Mesa
 		enable = true;
-
-		# Vulkan
 		enable32Bit = true;
-
-		extraPackages = with pkgs; [
-			rocmPackages.clr.icd
-		];
+		#		extraPackages = with pkgs; [
+		#			rocmPackages.clr.icd
+		#		];
 	};
+
+	systemd.services.lactd.wantedBy = ["multi-user.target"];
+	environment.systemPackages = with pkgs; [ lact ];
+	systemd.packages = with pkgs; [ lact ];
 
 	services = {
 		ollama = {
