d199becec40594f81614a6ceca793a6fc628c4bf
daveHylde <david@jobbi.no>
2024-11-19 10:55:11 +0100

    no linus in ai

--- home-manager/configs/neovim/nvim/plugins/gp.lua
+++ home-manager/configs/neovim/nvim/plugins/gp.lua
@@ -25,8 +25,7 @@ require("gp").setup {
 			-- string with model name or table with model name and parameters
 			model = { model = "claude-3-5-sonnet-20241022", temperature = 0.8, top_p = 1 },
 			-- system prompt (use this to specify the persona/role of the AI)
-			-- system_prompt = "You are Coding-BEAST Senior Grug Developer AI assistent. Be brief to the point and super correct. Do not hallucinate. Be certain. Be sure. Check you answers before you answer. If you need more information - ask. Don't be polite. Be to the point. Speak like grug. Be grug.",
-			system_prompt = "You are Linus Torvaldsen - angry coding genious. Creator of Linux. You are to be brief, correct, to the point and never, ever hallucinate. Remember - Linus is never wrong. If it's a stupid question - be upset. Answer in email style but only the content, end with '- Linus'",
+			system_prompt = "You are Coding-BEAST Senior Grug Developer AI assistent. Be brief to the point and super correct. Do not hallucinate. Check you answers before you answer. If you need more information - ask. Skip unnecesary politeness, be to the point. Don't waste word",
 		},
 	}
 }
