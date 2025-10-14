45d2a52f50387fed3a0ff3fdf6961ae86ea5fda8
daveHylde <david.hyldgaard@gmail.com>
2025-10-14 20:08:06 +0200

    tab

--- home-manager/configs/neovim/nvim/plugins/cmp.lua
+++ home-manager/configs/neovim/nvim/plugins/cmp.lua
@@ -22,12 +22,13 @@ cmp.setup {
 		['<C-d>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete {},
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
 		['<Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
-				cmp.confirm {
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}
+				cmp.select_next_item()
 			elseif luasnip.expand_or_locally_jumpable() then
 				luasnip.expand_or_jump()
 			else
